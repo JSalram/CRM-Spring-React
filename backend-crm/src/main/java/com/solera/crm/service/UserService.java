49ff7eba1eca9c5aa3afca4acff8376698c29426
Javi <javisr997@gmail.com>
2022-11-29 16:02:25 +0100

    Login completed

--- backend-crm/src/main/java/com/solera/crm/service/UserService.java
+++ backend-crm/src/main/java/com/solera/crm/service/UserService.java
@@ -1,8 +1,13 @@
 package com.solera.crm.service;
 
+import com.solera.crm.entity.User;
 import com.solera.crm.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UserService {
@@ -16,6 +21,18 @@ public class UserService {
 
     // Checks if user exists and log in
     public boolean login(String username, String password) {
-        return true;
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return false;
+        }
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        String hashedPassword = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+        User user = repo.findByUsernameAndPassword(username, hashedPassword);
+        return user != null;
     }
 }
