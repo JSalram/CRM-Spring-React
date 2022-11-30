30c76939f30037a2661e2f5b0b237c2ddfa6ea25
Javi <javisr997@gmail.com>
2022-11-30 10:28:02 +0100

    Refactored function for login according to the tests and generating token

--- backend-crm/src/main/java/com/solera/crm/service/UserService.java
+++ backend-crm/src/main/java/com/solera/crm/service/UserService.java
@@ -8,12 +8,23 @@ import org.springframework.stereotype.Service;
 import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Map;
 
 @Service
 public class UserService {
 
     private final UserRepository repo;
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
+
+    // Generates token for User
+    public static String generateNewToken() {
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
 
     @Autowired
     public UserService(UserRepository userRepository) {
@@ -22,6 +33,11 @@ public class UserService {
 
     // Checks if user exists and log in
     public User login(Map<String, String> data) {
+        User user = getUser(data);
+        if (user != null) {
+            user.setToken(generateNewToken());
+            this.repo.flush();
+        }
         return getUser(data);
     }
 
